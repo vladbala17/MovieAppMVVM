c7537fe8936301684cd91e69d90d4ae1a808529d
vladbalaneanu <vlad.balaneanu@shopkick.com>
2019-05-21 20:52:34 +0300

    created custom UI for movie list

--- app/src/main/java/com/android/vlad/movieapparchitecturecomponents/data/repository/remote/MoviesWebService.java
+++ app/src/main/java/com/android/vlad/movieapparchitecturecomponents/data/repository/remote/MoviesWebService.java
@@ -8,11 +8,12 @@ import retrofit2.http.Query;
 
 public interface MoviesWebService {
 
-    String BASE_URL = "https://api.themoviedb.org/3/movie/";
+    String BASE_URL = "https://api.themoviedb.org/3/";
+    String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w500";
 
-    @GET("top_rated")
+    @GET("movie/top_rated")
     Call<MovieResponse> getTopRatedMovies(@Query("api_key") String key);
 
-    @GET("{movie_id}")
+    @GET("movie/{movie_id}")
     Call<Movie> getMovieById(@Path("movie_id") String movieId, @Query("api_key") String key);
 }
--- app/src/main/java/com/android/vlad/movieapparchitecturecomponents/view/movielist/CustomBindingAdapter.java
+++ app/src/main/java/com/android/vlad/movieapparchitecturecomponents/view/movielist/CustomBindingAdapter.java
@@ -1,11 +1,39 @@
 package com.android.vlad.movieapparchitecturecomponents.view.movielist;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.databinding.BindingAdapter;
+import androidx.databinding.InverseBindingAdapter;
+import com.android.vlad.movieapparchitecturecomponents.data.repository.remote.MoviesWebService;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class CustomBindingAdapter {
     @BindingAdapter("visibleGone")
     public static void showHide(View view, boolean show) {
         view.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+    @BindingAdapter("android:text")
+    public static void setFloat(TextView view, float value) {
+        if (Float.isNaN(value)) view.setText("");
+        else view.setText(String.valueOf(value));
+    }
+
+    @InverseBindingAdapter(attribute = "android:text")
+    public static float getFloat(TextView view) {
+        String num = view.getText().toString();
+        if(num.isEmpty()) return 0.0F;
+        try {
+            return Float.parseFloat(num);
+        } catch (NumberFormatException e) {
+            return 0.0F;
+        }
+    }
+
+    @BindingAdapter("bind:imageUrl")
+    public static void loadImage(ImageView view, String url) {
+        Glide.with(view.getContext()).applyDefaultRequestOptions(RequestOptions.circleCropTransform()).load(MoviesWebService.IMAGE_BASE_URL + url).into(view);
     }
 }
